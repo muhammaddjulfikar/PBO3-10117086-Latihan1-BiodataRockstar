e95371031de44dc84e9ffa62f62a87bd59c40ece
djulfikar <muhammaddjulfikar7@gmail.com>
2018-09-23 22:52:52 -0700

    BiodataRockstar

---
+++ src/pbo3/pkg10117086/latihan1/biodatarockstar/PBO310117086Latihan1BiodataRockstar.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117086.latihan1.biodatarockstar;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class PBO310117086Latihan1BiodataRockstar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR " + "\n" + "---------------");
+        System.out.println("Naama\t\t: RD Muhammad Djulfkar BU");
+        System.out.println("Kelas\t\t: IF3");
+        System.out.println("NIM\t\t: 10117086");
+        System.out.println("Alamat\t: Jl Cikajang Antapani Bandung");
+        System.out.println("Tanggal Lahir\t: 11 Juli 1999");
+        System.out.println("======================");
+        System.out.println("Hobi\t\t: Tidur");
+        System.out.println("Makes\t\t: Sate");
+        System.out.println("Mikes\t\t: Ektrajos Campur Susu");
+    }
+    
+}
